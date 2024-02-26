7dab467bdd72b30bf65beecd5865d06e56adee4b
Kozlov Georgy <workKozlov@yandex.ru>
2024-02-27 00:33:30 +0100

    update words

--- src/app/data/sections/english/index.ts
+++ src/app/data/sections/english/index.ts
@@ -1,4 +1,5 @@
 import introductoryWords from 'src/app/data/sections/english/introductory-words';
+import lesson14Febrary from 'src/app/data/sections/english/lesson-14-febrary';
 import pronouns from 'src/app/data/sections/english/pronouns';
 import test from 'src/app/data/sections/english/test';
 import verbs from 'src/app/data/sections/english/verbs';
@@ -11,6 +12,7 @@ export const English: Section = {
     introductoryWords,
     verbs,
     pronouns,
-    test
+    test,
+    lesson14Febrary
   ],
 };
---
+++ src/app/data/sections/english/lesson-14-febrary.ts
@@ -0,0 +1,59 @@
+import { Category } from 'src/app/interfaces/category.interface';
+
+export default {
+  name: 'Урок от 14го февраля',
+  cards: [
+    {
+      question: 'especially',
+      answer: 'особенно'
+    },
+    {
+      question: 'age-mates',
+      answer: 'ровесники'
+    },
+    {
+      question: 'degree',
+      answer: 'степень'
+    },
+    {
+      question: 'to doubt',
+      answer: 'сомневаться'
+    },
+    {
+      question: 'once',
+      answer: 'как только'
+    },
+    {
+      question: 'adolescence',
+      answer: 'юность/молодость/подростковый возраст'
+    },
+    {
+      question: 'pressure',
+      answer: 'давление'
+    },
+    {
+      question: 'to lead',
+      answer: 'приводить'
+    },
+    {
+      question: 'to intend',
+      answer: 'предназначать'
+    },
+    {
+      question: 'reinforce',
+      answer: 'усиливаться'
+    },
+    {
+      question: 'let’s say',
+      answer: 'допустим'
+    },
+    {
+      question: 'stick to',
+      answer: 'придерживаться'
+    },
+    {
+      question: 'workforce',
+      answer: 'рабочая сила/трудовые ресурсы'
+    },
+  ]
+} as Category;
